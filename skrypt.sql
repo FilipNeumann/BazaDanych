1604ebf47ca8d111091dda2ad4b242e972a7153d
Kaseper9991 <49002663+Kaseper9991@users.noreply.github.com>
2019-05-14 20:45:47 +0200

    Update skrypt.sql

--- skrypt.sql
+++ skrypt.sql
@@ -208,7 +208,6 @@ CREATE TABLE `products` (
   `productCode` varchar(15) NOT NULL,
   `productName` varchar(70) NOT NULL,
   `productLine` varchar(50) NOT NULL,
-  `productScale` varchar(10) NOT NULL,
   `productVendor` varchar(50) NOT NULL,
   `productDescription` text NOT NULL,
   `quantityInStock` smallint(6) NOT NULL,
